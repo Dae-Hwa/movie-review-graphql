d1c5168b924c42f5764f0f1b1d6920bd20c48fa2
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2019-07-13 19:46:56 +0900

    Create schema.js

---
+++ server/src/schema.js
@@ -0,0 +1,32 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Movie {
+    imdbID: String
+    title: String
+    year: Int
+    poster: Int
+    released: String
+    director: String
+    actors: [String]
+    plot: String
+    ratings: [Rating]
+    averageRating: Float
+  }
+
+  type Rating {
+    rating: Int
+    comment: String
+  }
+
+  type Query {
+    movies: [Movie]
+    moive(imdbID: String!): Movie
+  }
+
+  type Mutation {
+    rating(imdbID: String!, rating: Int!, comment: String!): Movie
+  }
+`;
+
+module.exports = typeDefs;
